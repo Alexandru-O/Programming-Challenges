297a0684b03ef134b62641da451e2924eb38ad25
alex <ologu.alexandru@gmail.com>
2022-11-18 12:13:46 +0200

    Proj-003

---
+++ Challanges/003/Proj-003/Program.cs
@@ -0,0 +1,26 @@
+﻿int firstNumber;
+int secondNumber;
+
+try
+{
+    Console.Write("Please insert the first number: ");
+    firstNumber = int.Parse(Console.ReadLine());
+
+    Console.Write("Please insert the second number: ");
+    secondNumber = int.Parse(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Please insert only numbers");
+    /*-- exit --*/
+    Console.WriteLine("Press Enter to exit");
+    Console.Read();
+
+    return;
+}
+
+Console.WriteLine($"Divided result: {((double)firstNumber / secondNumber):N2}");
+
+/*-- exit --*/
+Console.WriteLine("Press Enter to exit");
+Console.Read();
