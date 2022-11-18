70601bec751600963bfef4cbbfac17d4e7450bf6
alex <ologu.alexandru@gmail.com>
2022-11-18 10:51:17 +0200

    Proj-002

---
+++ Challanges/002/Proj-002/Program.cs
@@ -0,0 +1,26 @@
+﻿int firstNumber;
+int secondNumber;
+
+try
+{
+    Console.Write("Please insert the first number: ");
+    firstNumber = int.Parse(Console.ReadLine());
+
+    Console.Write("Please insert the second number: ");
+    secondNumber = int.Parse(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Insert only numbers");
+    /*-- exit --*/
+    Console.WriteLine("Press Enter to exit");
+    Console.Read();
+
+    return;
+}
+
+Console.WriteLine($"The sum: {firstNumber + secondNumber}");
+
+/*-- exit --*/
+Console.WriteLine("Press Enter to exit");
+Console.Read();
