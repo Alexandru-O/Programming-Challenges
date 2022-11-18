2fdc03624cf160ee040968ae2c1fb1d1369ff1a6
alex <ologu.alexandru@gmail.com>
2022-11-18 10:28:52 +0200

    001

---
+++ Challanges/001/Proj-001/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Please enter your full name: ");
+
+string fullName = Console.ReadLine();
+
+Console.WriteLine($"Hello\n{fullName}");
+
+
+Console.WriteLine("Press enter to close...");
+Console.ReadLine();
+
