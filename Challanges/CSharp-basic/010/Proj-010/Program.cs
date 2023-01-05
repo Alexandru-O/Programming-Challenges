680bc4a039da55cba774c1477d01655447e5f218
alex <ologu.alexandru@gmail.com>
2023-01-05 12:55:10 +0200

    challenge 10

---
+++ Challanges/CSharp-basic/010/Proj-010/Program.cs
@@ -0,0 +1,26 @@
+﻿int firstNumber, secondNumber, thirdNumber;
+
+try
+{
+    Console.Write("Enter the first number: ");
+    firstNumber = int.Parse(Console.ReadLine());
+    Console.Write("Enter the second number: ");
+    secondNumber= int.Parse(Console.ReadLine());
+    Console.Write("Enter the third number: ");
+    thirdNumber= int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"({firstNumber} + {secondNumber}) * {thirdNumber} = {(firstNumber + secondNumber) * thirdNumber}");
+    Console.WriteLine($"{firstNumber} * {secondNumber} + {secondNumber} * {thirdNumber} = {firstNumber * secondNumber + secondNumber * thirdNumber}");
+}
+catch
+{
+    Console.WriteLine("Please insert only numbers!");
+    /*-- exit --*/
+    Console.WriteLine("Press Enter to exit");
+    Console.Read();
+
+    return;
+}
+/*-- exit --*/
+Console.WriteLine("Press Enter to exit");
+Console.Read();
