893e35037e8a58be9028c571d99751c52195c85f
alex <ologu.alexandru@gmail.com>
2023-01-05 12:27:26 +0200

    more challenges

---
+++ Challanges/CSharp-basic/008/Proj-008/Program.cs
@@ -0,0 +1,25 @@
+﻿int number;
+
+try
+{
+    Console.Write("Enter the number: ");
+    number = int.Parse(Console.ReadLine());
+
+    for(int i = 0; i <= 10; i++)
+    {
+        Console.WriteLine($"{number} * {i} = {number * i}");
+    }
+}
+catch
+{
+    Console.WriteLine("Please insert only numbers!");
+    /*-- exit --*/
+    Console.WriteLine("Press Enter to exit");
+    Console.Read();
+
+    return;
+}
+
+/*-- exit --*/
+Console.WriteLine("Press Enter to exit");
+Console.Read();
---
+++ Challanges/CSharp-basic/009/Proj-009/Program.cs
@@ -0,0 +1,30 @@
+﻿int firstNumber, secondNumber, thirdNumber, fourthNumber, avgNumber;
+
+try
+{
+    Console.Write("Enter the first number: ");
+    firstNumber = int.Parse(Console.ReadLine());
+    Console.Write("Enter the second number: ");
+    secondNumber = int.Parse(Console.ReadLine());
+    Console.Write("Enter the third number: ");
+    thirdNumber = int.Parse(Console.ReadLine());
+    Console.Write("Enter the fourth number: ");
+    fourthNumber = int.Parse(Console.ReadLine());
+
+    avgNumber = (firstNumber+ secondNumber + thirdNumber + fourthNumber) / 4;
+
+    Console.WriteLine($"The average of {firstNumber}, {secondNumber}, {thirdNumber}, {fourthNumber} is {avgNumber}");
+}
+catch
+{
+    Console.WriteLine("Please insert only numbers!");
+    /*-- exit --*/
+    Console.WriteLine("Press Enter to exit");
+    Console.Read();
+
+    return;
+}
+
+/*-- exit --*/
+Console.WriteLine("Press Enter to exit");
+Console.Read();
