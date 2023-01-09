1c460a6909c2962af48f28f953d9843eb5fb7de3
alex <ologu.alexandru@gmail.com>
2023-01-09 15:07:57 +0200

    014

---
+++ Challanges/CSharp-basic/013/Proj-013/Program.cs
@@ -0,0 +1,24 @@
+﻿int number;
+try
+{
+    Console.Write("Enter a number: ");
+    number = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("{0}{0}{0}", number);
+    Console.WriteLine("{0} {0}", number);
+    Console.WriteLine("{0} {0}", number);
+    Console.WriteLine("{0} {0}", number);
+    Console.WriteLine("{0}{0}{0}", number);
+}
+catch
+{
+    Console.WriteLine("Please insert a number!");
+    /*-- exit --*/
+    Console.WriteLine("Press Enter to exit");
+    Console.Read();
+
+    return;
+}
+/*-- exit --*/
+Console.WriteLine("Press Enter to exit");
+Console.Read();
---
+++ Challanges/CSharp-basic/014/Proj-014/Program.cs
@@ -0,0 +1,21 @@
+﻿int temp;
+try
+{
+    Console.Write("Enter the amount of celsius: ");
+    temp = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Kelvin = {temp + 273}");
+    Console.WriteLine($"Fahrenheit = {temp * 1.8 + 32}");
+}
+catch
+{
+    Console.WriteLine("Temperature must be a number!");
+    /*-- exit --*/
+    Console.WriteLine("Press Enter to exit");
+    Console.Read();
+
+    return;
+}
+/*-- exit --*/
+Console.WriteLine("Press Enter to exit");
+Console.Read();
