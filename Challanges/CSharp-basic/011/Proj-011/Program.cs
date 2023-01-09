2ba030d05fdafda93d162eccef7d170568e9bc9a
alex <ologu.alexandru@gmail.com>
2023-01-09 07:47:04 +0200

    012

---
+++ Challanges/CSharp-basic/011/Proj-011/Program.cs
@@ -0,0 +1,20 @@
+﻿int age;
+try
+{
+    Console.Write("Please insert your age: ");
+    age = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"You look older then {age}!");
+}
+catch
+{
+    Console.WriteLine("Age must be a number!");
+    /*-- exit --*/
+    Console.WriteLine("Press Enter to exit");
+    Console.Read();
+
+    return;
+}
+/*-- exit --*/
+Console.WriteLine("Press Enter to exit");
+Console.Read();
---
+++ Challanges/CSharp-basic/012/Proj-012/Program.cs
@@ -0,0 +1,41 @@
+﻿int number;
+try
+{
+    Console.Write("Enter a digit: ");
+    number = int.Parse(Console.ReadLine());
+
+    /*-- first part with Console.Write --*/
+    Console.Write(number);
+    Console.Write(' ');
+    Console.Write(number);
+    Console.Write(' ');
+    Console.Write(number);
+    Console.Write(' ');
+    Console.Write(number);
+    Console.Write('\n');
+
+    Console.Write(number);
+    Console.Write(number);
+    Console.Write(number);
+    Console.Write(number);
+    Console.Write('\n');
+
+    /*-- second part with Console.Write --*/
+
+    Console.WriteLine("{0} {0} {0} {0}", number);
+    Console.WriteLine("{0}{0}{0}{0}",number);
+
+
+}
+catch
+{
+    Console.WriteLine("Please insert only numbers!");
+    /*-- exit --*/
+    Console.WriteLine("Press Enter to exit");
+    Console.Read();
+
+    return;
+}
+/*-- exit --*/
+Console.WriteLine("Press Enter to exit");
+Console.Read();
